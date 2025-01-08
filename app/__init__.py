dcab7e28687db79e0e407282874fca0d15636988
Roua91 <165356652+Roua91@users.noreply.github.com>
2025-01-08 12:33:38 +0800

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+'''
+Purpose: 
+1) Initializes the Flask app. 
+2) Sets up configurations, 
+3) database connections and extensions like Flask-Migrate or Flask-Login.
+4) Registers blueprints for modular routes.
+'''
